c50d2e3b14f3b5ec1a9339b8f6d4cbb028918b34
Smail Ali <sami@samis-imac.local>
2016-02-28 18:33:55 -0500

    completed part 3

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -19,15 +19,32 @@ class APIManager {
         
         let task = session.dataTaskWithURL(url!){
             (data, response, error) -> Void in
-            
-            dispatch_async(dispatch_get_main_queue()){
+
                 if error != nil {
-                    completion(result: (error!.localizedDescription))
+                    dispatch_async(dispatch_get_main_queue()){
+                        completion(result: (error!.localizedDescription))
+                    }
                 }else {
-                    completion(result: "NSURLSEssion successful")
+                    do {
+                        if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject]{
+                            print(json)
+                            
+                            let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                            dispatch_async(dispatch_get_global_queue(priority, 0)){
+                                dispatch_async(dispatch_get_main_queue()){
+                                    completion(result: "JSONSerilization Successful")
+                                }
+                                
+                            }
+                        }
+                    } catch{
+                        dispatch_async(dispatch_get_main_queue()){
+                            completion(result: "error in NSJSONSerialization")
+                        }
+                    }
                 
                 }
-            }
+            
         }
         task.resume()
         
