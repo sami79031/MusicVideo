85ddc0d5e8e26bcb44182df2533ca49aba85226d
Smail Ali <sami@samis-imac.local>
2016-02-28 19:04:05 -0500

    part 4completed

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -26,7 +26,7 @@ class APIManager {
                     }
                 }else {
                     do {
-                        if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject]{
+                        if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? JSONDictionary{
                             print(json)
                             
                             let priority = DISPATCH_QUEUE_PRIORITY_HIGH
---
+++ MusicVideo/Contsants.swift
@@ -0,0 +1,13 @@
+//
+//  Contsants.swift
+//  MusicVideo
+//
+//  Created by Smail Ali on 2/28/16.
+//  Copyright © 2016 Smail Ali. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String : AnyObject]
+
+typealias JSONArray = Array<AnyObject>
