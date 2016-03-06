423ba00cdc29cc0d9de7fb8bbeb15ad5e93cf1f2
Smail Ali <sami@samis-imac.local>
2016-03-06 18:42:15 -0500

    part 11 completed

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -8,15 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var displayLable: UILabel!
     var videos = [Videos]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let api = APIManager()
-        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json", completion: didLoadData)
+        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=50/json", completion: didLoadData)
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "reachStatusChanged", name: "ReachStatusChanged", object: nil)
         reachStatusChanged()
@@ -24,20 +24,52 @@ class ViewController: UIViewController {
 
 
     func didLoadData(videos: [Videos]){
-        
+        self.videos = videos
         print(reachabilityStatus)
         
         for (index, item) in videos.enumerate(){
             print("\(index + 1) \(item.vName)")
         }
+        
+        tableView.reloadData()
     }
     
     func reachStatusChanged(){
-        displayLable.text = reachabilityStatus
+        switch reachabilityStatus {
+        case NOACCESS : view.backgroundColor = UIColor.redColor()
+        displayLable.text = "No Internet"
+        case WIFI : view.backgroundColor = UIColor.greenColor()
+        displayLable.text = "Reachable with WIFI"
+        case WWAN : view.backgroundColor = UIColor.yellowColor()
+        displayLable.text = "Reachable with Cellular"
+        default:return
+        }
     }
     
     deinit{
         NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int{
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        
+        let video = videos[indexPath.row]
+        
+        cell.textLabel?.text = ("\(indexPath.row + 1)")
+        
+        cell.detailTextLabel?.text = video.vName
+        
+        return cell
+        
     }
 
 }
